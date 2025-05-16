f9d323971d32282d50578effc48235cbfa847d13
Risto Virtaharju <97247176+rvirtaha@users.noreply.github.com>
2025-05-16 17:56:10 +0300

    Dev (#20)
    feat(vis): added favicons
    
    docs(vis): Add usage instructions for favicons
    
    docs: write README
    
    feat(tw): add tailwind plugin

--- packages/visual-assets/index.js
+++ packages/visual-assets/index.js
@@ -1,2 +1,17 @@
+import appleTouch from './dist/favicons/apple-touch-icon.png'
+import favicon96 from './dist/favicons/favicon-96x96.png'
+import faviconSvg from './dist/favicons/favicon.svg'
+import faviconIco from './dist/favicons/favicon.ico'
+import webmanifest from './dist/favicons/site.webmanifest'
+
+const favicons = {
+    appleTouch,
+    favicon96,
+    faviconSvg,
+    faviconIco,
+    webmanifest,
+}
+
 export { default as logos } from './dist/logos.svg'
 export { default as icons } from './dist/icons.svg'
+export { favicons }
